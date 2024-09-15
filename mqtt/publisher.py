b4bbdfa4d7bfbf8b42baa1245b119848d1c8b4d2
nihalsuri <37659632+nihalsuri@users.noreply.github.com>
2024-09-15 19:05:23 +0200

    updated publisher to match all basic functionality

--- mqtt/publisher.py
+++ mqtt/publisher.py
@@ -1,26 +1,19 @@
+import json
 import paho.mqtt.client as mqtt
 
-# Define the broker details
-broker = "test.mosquitto.org"
-port = 1883
-topic = "m/plc/sensor"
 
-# Callback when the message has been published
-def on_publish(client, userdata, result):
-    print("Message Published")
+class MQTTPublisher:
+    def __init__(self, broker="test.mosquitto.org", port=1883, topic="m/plc/sensor"):
+        self.broker = broker
+        self.port = port
+        self.topic = topic
+        self.client = mqtt.Client()
 
-# Create an MQTT client instance
-client = mqtt.Client()
+        # Connect to the broker
+        self.client.connect(self.broker, self.port)
 
-# Attach the on_publish callback function
-client.on_publish = on_publish
-
-# Connect to the broker
-client.connect(broker, port)
-
-# Publish a message
-message = "Hello from MQTT publisher!"
-client.publish(topic, message)
-
-# Disconnect from the broker
-client.disconnect()
+    def publish_message(self, message):
+        """Publish the given message (as a JSON string) to the MQTT broker."""
+        message_json = json.dumps(message)
+        self.client.publish(self.topic, message_json)
+        print("Message Published:", message_json)
